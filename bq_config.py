c416183a79c705418ad545d0c7def56e316b284c
saviornt <david.wadsworth80@gmail.com>
2022-10-13 00:45:11 -0700

    Update bq_config.py

--- bq_config.py
+++ bq_config.py
@@ -4,9 +4,8 @@ import os
 # Variables
 
 # The project_id string should be the Project ID or Project Name from the Google Cloud console
-project_id = "test-project-365403"
+project_id = "project-id-string"
 
 # This string should be the directory where the service account key from Google Cloud is stored
-#json_path = "path_to_json_file"
-json_path = "/home/defaultuser/Projects/BigQuery/creds.json"
+json_path = "/home/xxx/Projects/BigQuery/creds.json"
 os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = json_path
