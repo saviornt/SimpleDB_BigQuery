1c5dda7c443fe2656289a4da771be3c43effcf84
saviornt <david.wadsworth80@gmail.com>
2022-10-13 18:59:35 -0700

    10/13/2022 - Project Start

--- main.py
+++ main.py
@@ -4,7 +4,17 @@ from simpleDB import Db
 
 project_id = bq_config.project_id
 if __name__ == '__main__':
-    # Db.create_database("test", "US")
-    # Db.copy_database("test", project_id, "test2")
+    db_name = "test"
+    db_location = "US"
+    db_description = "Just a test dataset"
+    # Db.create_database(db_name, db_location, db_description)
+    # Db.copy_database(db_name, project_id, "test2")
     # Db.list_databases()
-    # Db.delete_database("test2")
+    # Db.delete_database("test")
+    field_count = 2
+    table_name = "test_table"
+    table_description = "This is a test table for testing"
+    table_expires = 0
+    Db.delete_table(db_name, table_name)
+    #schema = Db.create_schema(field_count)
+    #Db.create_table(db_name, table_name, schema, table_description,table_expires)
--- simpleDB.py
+++ simpleDB.py
@@ -1,4 +1,4 @@
-import bq_config
+import bq_config, datetime
 from google.cloud import bigquery
 from google.cloud import bigquery_datatransfer
 
@@ -11,6 +11,11 @@ regions = ["US", "us-central1", "us-west4", "us-west2", "northamerica-northeast1
            "asia-northeast1", "europe-west1", "europe-north1", "europe-west3",
            "europe-west2", "europe-southwest1", "europe-west8", "europe-west4",
            "europe-west9", "europe-central2", "europe-west6"]
+
+field_types = ["INT64", "FLOAT64", "NUMERIC", "BIGNUMERIC", "BOOL", "STRING", "BYTES", "DATE", "DATETIME", "TIME",
+               "TIMESTAMP", "STRUCT", "GEOGRAPHY", "JSON"]
+
+field_modes = ["NULLABLE", "REQUIRED", "REPEATED"]
 
 
 class Db:
@@ -26,14 +31,15 @@ class Db:
         return client
 
     @staticmethod
-    def create_database(name, location):
+    def create_database(database_name, location, description=""):
         # TODO: Create DOCSTRING
         """
         See (https://cloud.google.com/bigquery/docs/locations) for a list of valid locations
         """
+
         if location not in regions:
-            print("Invalid location for database.")
-            return
+            err = "Invalid location for database."
+            return err
 
         client = Db.connect()
         datasets = list(client.list_datasets())
@@ -41,20 +47,22 @@ class Db:
         for dataset in datasets:
             db_list.append("{}".format(dataset.dataset_id))
 
-        if name in db_list:
-            print("A database named {} already exists.".format(name))
+        if database_name in db_list:
+            err = "A database named {} already exists.".format(database_name)
+            return err
 
         else:
-            dataset_id = "{}.{}".format(client.project, name)
+            dataset_id = "{}.{}".format(client.project, database_name)
             dataset = bigquery.Dataset(dataset_id)
             dataset.location = location
+            dataset.description = description
             dataset = client.create_dataset(dataset, timeout=30)
-            print("Created dataset {}.{}".format(client.project, dataset.dataset_id))
-
-        return
+            dataset = client.update_dataset(dataset, ["description"])
+            result = "Created dataset {}.{}".format(client.project, dataset.dataset_id)
+        return result
 
     @staticmethod
-    def copy_database(source_databaseName, copy_projectName, copy_databaseName):
+    def copy_database(source_database_name, copy_project_name, copy_database_name):
         # TODO: Create DOCSTRING
         # First we need to check if the source dataset actually exists for it to be copied.
         client = Db.connect()
@@ -64,35 +72,36 @@ class Db:
         for dataset in datasets:
             db_list.append("{}".format(dataset.dataset_id))
 
-        if source_databaseName not in db_list:
-            print("A database named {} does exist in the project to be copied.".format(source_databaseName))
-
+        if source_database_name not in db_list:
+            err = "A database named {} does exist in the project to be copied.".format(source_database_name)
+            return err
         else:
             # Since the source is legit, we need to check to see if the destination dataset already exists
             db_list = []
             for dataset in datasets:
                 db_list.append("{}".format(dataset.dataset_id))
-            if copy_databaseName in db_list:
-                print("The {} database already exists.".format(copy_databaseName))
+            if copy_database_name in db_list:
+                err = "The {} database already exists.".format(copy_database_name)
+                return err
             else:
                 source_project_id = bq_config.project_id
                 transfer_client = bigquery_datatransfer.DataTransferServiceClient()
                 transfer_config = bigquery_datatransfer.TransferConfig(
-                    destination_dataset_id=copy_databaseName,
-                    display_name=copy_databaseName,
+                    destination_dataset_id=copy_database_name,
+                    display_name=copy_database_name,
                     data_source_id="cross_region_copy",
                     params={
                         "source_project_id": source_project_id,
-                        "source_dataset_id": source_databaseName,
+                        "source_dataset_id": source_database_name,
                     },
                     schedule="every 24 hours",
                 )
                 transfer_config = transfer_client.create_transfer_config(
-                    parent=transfer_client.common_project_path(copy_projectName),
+                    parent=transfer_client.common_project_path(copy_project_name),
                     transfer_config=transfer_config,
                 )
-                print(f"Created copy configuration: {transfer_config.name}")
-        return
+                result = f"Created copy configuration: {transfer_config.name}"
+        return result
 
     @staticmethod
     def list_databases():
@@ -100,30 +109,129 @@ class Db:
         client = Db.connect()
         project = client.project
         datasets = list(client.list_datasets())
-
+        db_list = []
         if datasets:
-            print("Databases in project {}:".format(project))
             for dataset in datasets:
-                print("\t{}".format(dataset.dataset_id))
-        else:
-            print("{} project does not contain any databases.".format(project))
+                db_list.append("{}".format(dataset.dataset_id))
+        return db_list
 
+    @staticmethod
+    def update_database_description(database_name, database_description):
+        client = Db.connect()
+        dataset_id = "{}.{}".format(project_id, database_name)
+        dataset = client.get_dataset(dataset_id)
+        dataset.description = database_description
+        dataset = client.update_dataset(dataset, ["description"])
         return
 
     @staticmethod
-    def delete_database(name):
+    def delete_database(database_name):
         # TODO: Create DOCSTRING
         client = Db.connect()
-        dataset_id = "{}.{}".format(project_id, name)
+        dataset_id = "{}.{}".format(project_id, database_name)
         datasets = list(client.list_datasets())
         db_list = []
 
         for dataset in datasets:
             db_list.append("{}".format(dataset.dataset_id))
-        if name in db_list:
+        if database_name in db_list:
             client.delete_dataset(dataset_id, delete_contents=True, not_found_ok=True)
-            print("Deleted database '{}'.".format(dataset_id))
+            result = "Deleted database '{}'.".format(dataset_id)
         else:
-            print("The database was not found in {}.".format(project_id))
+            result = "The database was not found in {}.".format(project_id)
 
-        return
+        return result
+
+    # This method may not be needed, re-evaluate
+    @staticmethod
+    def create_field(field_name, field_type, field_mode):
+        # TODO: Create DOCSTRING
+        field_type = field_type.upper()
+        field_mode = field_mode.upper()
+        if field_type not in field_types:
+            err = "Invalid schema field type."
+            return err
+        if field_mode not in field_modes:
+            err = "Invalid schema field mode."
+            return err
+        schema_field = "{}, {}, mode={}".format(field_name, field_type, field_mode)
+        return schema_field
+
+    @staticmethod
+    def create_schema(total_fields):
+        schema = []
+        for i in range(total_fields):
+            field_name = input("Enter field name: ")
+            field_type = input("Enter field type: ").upper()
+            if field_type not in field_types:
+                print("Invalid field type.")
+                field_type = input("Enter field type: ").upper()
+            field_mode = input("Enter field mode: ").upper()
+            while field_mode not in field_modes:
+                print("Invalid field mode.")
+                field_mode = input("Enter field mode: ").upper()
+            schema.append(bigquery.SchemaField(field_name, field_type, field_mode))
+        return schema
+
+    @staticmethod
+    def create_table(database_name, table_name, schema, table_description="", table_expires=0):
+        # TODO: Create DOCSTRING
+        # table_expires is measured in days, if it is 0, then it does not expire
+
+        client = Db.connect()
+        table_id = "{}.{}.{}".format(project_id, database_name, table_name)
+        table = bigquery.Table(table_id, schema=schema)
+        table = client.create_table(table)
+        if table_description != "":
+            Db.update_table_description(database_name, table_name, table_description)
+        Db.update_table_expiration(database_name, table_name, table_expires)
+        result = "Created table {}.{}.{}".format(table.project, table.dataset_id, table.table_id)
+        return result
+
+    @staticmethod
+    def update_table_description(database_name, table_name, table_description):
+        client = Db.connect()
+        project = client.project
+        dataset_ref = bigquery.DatasetReference(project, database_name)
+        table_ref = dataset_ref.table(table_name)
+        table = client.get_table(table_ref)
+        table.description = table_description
+        table = client.update_table(table, ["description"])
+        result = "Updated table description to {}".format(table_description)
+        return result
+
+    @staticmethod
+    def update_table_expiration(database_name, table_name, table_expiration):
+        client = Db.connect()
+        project = client.project
+        dataset_ref = bigquery.DatasetReference(project, database_name)
+        table_ref = dataset_ref.table(table_name)
+        table = client.get_table(table_ref)
+
+        if table_expiration == 0:
+            table_expiration = "Never"
+            result = "Updated the table to never expire"
+        else:
+            table_expiration = datetime.datetime.now(datetime.timezone.utc) + datetime.timedelta(days=table_expiration)
+            result = "Updated the table to expire in {} days.".format(table_expiration)
+        table.expires = table_expiration
+        table = client.update_table(table, ["expires"])
+        return result
+
+    @staticmethod
+    def copy_table(source_database_name, source_table_name, destination_database_name, destination_table_name):
+        client = Db.connect()
+        source_table_id = "{}.{}.{}".format(project_id, source_database_name, source_table_name)
+        destination_table_id = "{}.{}.{}".format(project_id, destination_database_name, destination_table_name)
+        copy_job = client.copy_table(source_table_id, destination_table_id)
+        copy_job.result()
+        result = "The table {} has been copied to {}.".format(source_table_name, destination_database_name)
+        return result
+
+    @staticmethod
+    def delete_table(database_name, table_name):
+        client = Db.connect()
+        table_id = "{}.{}.{}".format(project_id, database_name, table_name)
+        client.delete_table(table_id, not_found_ok=True)
+        result = "Deleted table {}.".format(table_id)
+        return result
